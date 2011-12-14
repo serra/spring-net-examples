56431edf5a86d07ab3c6cf6fbfcbfcecfc323d54
Marijn van der Zee <marijn@serraict.com>
2011-12-14 16:58:30 +0100

    Add test skeleton
    Not a single test yet.

---
+++ Spring.IoCQuickStart.VariableSources/test/Spring.IocQuickStart.VariableSources.Test/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Example;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace Spring.IocQuickStart.IVariableSource.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void ListPeople()
+        {
+            // e.g.:
+            //var ctx = new XmlApplicationContext(@".\VariableSources\objects.xml");
+            //var people = (List<Person>)ctx["people"];
+            //foreach (var person in people)
+            //{
+            //    Console.WriteLine(person);
+            //}
+        }
+    }
+}
