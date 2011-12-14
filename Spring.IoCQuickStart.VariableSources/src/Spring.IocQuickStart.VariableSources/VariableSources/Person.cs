fdd07289125639b8fc1075048bfcf7853bc6eb09
Marijn van der Zee <marijn@serraict.com>
2011-12-14 15:59:38 +0100

    Add program skeleton

---
+++ Spring.IoCQuickStart.VariableSources/src/Spring.IocQuickStart.VariableSources/VariableSources/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Spring.IocQuickStart.VariableSources
+{
+    public class Person
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", Name, Age);
+        }
+    }
+}
---
+++ Spring.IoCQuickStart.VariableSources/src/Spring.IocQuickStart.VariableSources/VariableSources/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Spring.Context.Support;
+
+namespace Spring.IocQuickStart.VariableSources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new XmlApplicationContext(@".\VariableSources\objects.xml");
+            var people = (List<Person>) ctx["people"];
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+
+            Console.WriteLine("Done ...");
+            Console.ReadLine();
+        }
+    }
+}
