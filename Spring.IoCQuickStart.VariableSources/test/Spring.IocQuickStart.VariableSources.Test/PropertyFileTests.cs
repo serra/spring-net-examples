26930b519a2c9a2cbcb56c18d7368b073c65dfc7
Marijn van der Zee <marijn@serraict.com>
2011-12-29 16:19:38 +0100

    Add some tests for `RegistryVariableSource` and `PropertyFileVariableSource`

---
+++ Spring.IoCQuickStart.VariableSources/test/Spring.IocQuickStart.VariableSources.Test/PropertyFileTests.cs
@@ -0,0 +1,37 @@
+﻿using Example;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace Spring.IocQuickStart.IVariableSource.Test
+{
+    [TestFixture]
+    public class PropertyFileTests
+    {
+        [Test]
+        public void LastFileTakesPrecedence()
+        {
+            var ctx = new XmlApplicationContext(
+                    "assembly://Spring.IocQuickStart.VariableSource.Test/" + 
+                    "Spring.IocQuickStart.VariableSource.Test.PropertyFileTestFiles/" +
+                    "objects-file-precedence-tests.xml");
+
+            var joan = (Person) ctx["joan"];
+
+            // when using property files, the last loaded file takes precedence (can be considered a bug)
+            Assert.AreEqual("2 Joan Harris", joan.Name);
+        }
+
+        [Test]
+        public void LastEntryTakesPrecedence()
+        {
+            var ctx = new XmlApplicationContext(
+                    "assembly://Spring.IocQuickStart.VariableSource.Test/" +
+                    "Spring.IocQuickStart.VariableSource.Test.PropertyFileTestFiles/" +
+                    "objects-entry-precedence-tests.xml");
+            var joan = (Person)ctx["joan"];
+
+            // when using property files, the last loaded file takes precedence (can be considered a bug)
+            Assert.AreEqual("Using last entry Joan Harris", joan.Name);
+        }
+    }
+}
---
+++ Spring.IoCQuickStart.VariableSources/test/Spring.IocQuickStart.VariableSources.Test/RegistryVariableSourceTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Example;
+using Microsoft.Win32;
+using NUnit.Framework;
+using Spring.Context.Support;
+using System.Linq;
+using Spring.Objects.Factory;
+
+namespace Spring.IocQuickStart.IVariableSource.Test
+{
+    [TestFixture]
+    public class RegistryVariableSourceTests
+    {
+        private static RegistryKey _key;
+        private static string _subkey = "TestKeyForVariableSourcesTests";
+
+        [SetUp]
+        public void SetUp()
+        {
+            AddFreddyToRegistry();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            RemoveFreddyFromRegistry();
+        }
+
+        [Test]
+        public void ReadFromRegistryTest()
+        {
+            var ctx = new XmlApplicationContext(
+                    "assembly://Spring.IocQuickStart.VariableSource.Test/" +
+                    "Spring.IocQuickStart.VariableSource.Test.PropertyFileTestFiles/" +
+                    "objects-correctregistrykey.xml");
+            var freddy = (Person)ctx["freddy"];
+
+            Assert.AreEqual("Freddy Rumsen", freddy.Name);
+        }
+
+        [Test]
+        public void NonExistingKeyThrows()
+        {
+            Assert.False(Registry.CurrentUser.GetSubKeyNames().Contains("TestKeyDoesNotExist"));
+
+            try
+            {
+                var ctx = new XmlApplicationContext(
+                    "assembly://Spring.IocQuickStart.VariableSource.Test/" +
+                    "Spring.IocQuickStart.VariableSource.Test.PropertyFileTestFiles/" +
+                    "objects-nonexistingregistrykey.xml");
+            }
+            catch (ObjectCreationException oe)
+            {
+                Assert.Pass();
+            }
+
+        }
+
+
+        private static void AddFreddyToRegistry()
+        {
+            _key = Registry.CurrentUser.CreateSubKey(_subkey);
+            _key.SetValue("freddy_name", "Freddy Rumsen");
+            _key.SetValue("freddy_age", 44, RegistryValueKind.DWord);
+            _key.Flush();
+        }
+
+        private static void RemoveFreddyFromRegistry()
+        {
+            Registry.CurrentUser.DeleteSubKey(_subkey);
+        }
+    }
+}
